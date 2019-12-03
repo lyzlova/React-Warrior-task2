758ea7d5e1f6aa51b92ece9bd25234abcf601a02
Ivanna <lyzlovaivanna@gmail.com>
2019-12-03 14:08:51 +0200

    Updates

---
+++ precache-manifest.b059b420b8f8995e9444f17963016511.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab7e531325a97843e1f6954677144c5",
+    "url": "/React-Warrior-task2/index.html"
+  },
+  {
+    "revision": "389c209fa75a632cbc40",
+    "url": "/React-Warrior-task2/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "32125311d89af6749b3f",
+    "url": "/React-Warrior-task2/static/css/main.3453b0b6.chunk.css"
+  },
+  {
+    "revision": "389c209fa75a632cbc40",
+    "url": "/React-Warrior-task2/static/js/2.2156865f.chunk.js"
+  },
+  {
+    "revision": "32125311d89af6749b3f",
+    "url": "/React-Warrior-task2/static/js/main.31ac30b1.chunk.js"
+  },
+  {
+    "revision": "d358be7590423cf9338d",
+    "url": "/React-Warrior-task2/static/js/runtime-main.93b3117a.js"
+  },
+  {
+    "revision": "f065b10c4a756e686ac012fb675b334f",
+    "url": "/React-Warrior-task2/static/media/default-img.f065b10c.png"
+  }
+]);
